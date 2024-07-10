6036c89fd6a2b3e7c9a9b0359b4210fa32d69f72
ME KALIN BHAIYA <mdistkhar1078@gmail.com>
2024-07-10 15:04:16 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -3,7 +3,7 @@ from typing import List
 
 import yaml
 
-LOGGERS = "VEENA_HELP_BOT"  # connect errors api key "Dont change it"
+LOGGERS = "VeenaMusic_bot"  # connect errors api key "Dont change it"
 
 languages = {}
 languages_present = {}
